c840c1a5f74a7f7f1932ad6934372615a52773a8
Alfonso Guerra <huperniketes@gmail.com>
2017-07-09 05:53:52 -0400

    Extracted UrlPickerButton from the encapsulation violations (setting and handling click events) in MainActivity. Updated the eject button to use an instance of UrlPickerButton instead of Button.

--- src/com/huperniketes/oompa/MainActivity.java
+++ src/com/huperniketes/oompa/MainActivity.java
@@ -17,81 +17,24 @@
 package com.huperniketes.oompa;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
 
 /** 
  * Main activity: shows media player buttons. This activity shows the media player buttons and
  * lets the user click them. No media handling is done here -- everything is done by passing
  * Intents to our {@link MusicService}.
  * */
-public class MainActivity extends Activity implements OnClickListener {
-    /**
-     * The URL we suggest as default when adding by URL. This is just so that the user doesn't
-     * have to find an URL to test this sample.
-     */
-    final String SUGGESTED_URL = "http://www.vorbis.com/music/Epoq-Lepidoptera.ogg";
-
-    Button mEjectButton;
+public class MainActivity extends Activity {
 
     /**
-     * Called when the activity is first created. Here, we simply set the event listeners and
-     * start the background service ({@link MusicService}) that will handle the actual media
-     * playback.
+     * Called when the activity is first created. Here, we simply set the content view.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        mEjectButton = (Button) findViewById(R.id.ejectbutton);
-        mEjectButton.setOnClickListener(this);
-    }
-
-    public void onClick(View target) {
-    	
-        if(target == mEjectButton) {
-            showUrlDialog();
-        }
-    }
-
-    /** 
-     * Shows an alert dialog where the user can input a URL. After showing the dialog, if the user
-     * confirms, sends the appropriate intent to the {@link MusicService} to cause that URL to be
-     * played.
-     */
-    void showUrlDialog() {
-        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-        alertBuilder.setTitle("Manual Input");
-        alertBuilder.setMessage("Enter a URL (must be http://)");
-        final EditText input = new EditText(this);
-        alertBuilder.setView(input);
-
-        input.setText(SUGGESTED_URL);
-
-        alertBuilder.setPositiveButton("Play!", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dlg, int whichButton) {
-                // Send an intent with the URL of the song to play. This is expected by
-                // MusicService.
-                Intent i = new Intent(MusicService.ACTION_URL);
-                Uri uri = Uri.parse(input.getText().toString());
-                i.setData(uri);
-                startService(i);
-            }
-        });
-        alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dlg, int whichButton) {}
-        });
-
-        alertBuilder.show();
     }
 
     @Override
---
+++ src/com/huperniketes/oompa/ui/UrlPickerButton.java
@@ -0,0 +1,65 @@
+package com.huperniketes.oompa.ui;
+
+import com.huperniketes.oompa.MusicService;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class UrlPickerButton extends Button implements OnClickListener {
+
+	/**
+     * The URL we suggest as default when adding by URL. This is just so that the user doesn't
+     * have to find an URL to test this sample.
+     */
+    final String SUGGESTED_URL = "http://www.vorbis.com/music/Epoq-Lepidoptera.ogg";
+
+	public UrlPickerButton(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setOnClickListener((OnClickListener) this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		showUrlDialog();
+	}
+
+    /** 
+     * Shows an alert dialog where the user can input a URL. After showing the dialog, if the user
+     * confirms, sends the appropriate intent to the {@link MusicService} to cause that URL to be
+     * played.
+     */
+    void showUrlDialog() {
+    	final Context		context = getContext();
+    	AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setTitle("Manual Input");
+        alertBuilder.setMessage("Enter a URL (must be http://)");
+        final EditText input = new EditText(context);
+        alertBuilder.setView(input);
+
+        input.setText(SUGGESTED_URL);
+
+        alertBuilder.setPositiveButton("Play!", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dlg, int whichButton) {
+                // Send an intent with the URL of the song to play. This is expected by
+                // MusicService.
+                Intent i = new Intent(MusicService.ACTION_URL);
+                Uri uri = Uri.parse(input.getText().toString());
+                i.setData(uri);
+                context.startService(i);
+            }
+        });
+        alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dlg, int whichButton) {}
+        });
+
+        alertBuilder.show();
+    }
+}
